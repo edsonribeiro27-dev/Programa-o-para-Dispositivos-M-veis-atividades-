51d6eb8d8f90356c9608a0570b75e2d12b05bdef
edsonribeiro27-dev <edson.ribeiro62@aluno.ifce.edu.br>
2026-08-20 16:29:49 -0300

    Add Dart program to calculate differences of sums

---
+++ estruturas de repetições/q7.dart
@@ -0,0 +1,20 @@
+import 'dart:io';
+void main() {
+  print('Digite um número inteiro positivo: ');
+  int n = int.parse(stdin.readLineSync()!);
+
+  int soma = 0;
+  int somaDosQuadrados = 0;
+
+  for (int i = 1; i <= n; i++) {
+    soma += i;
+    somaDosQuadrados += i * i;
+  }
+  int quadradoDaSoma = soma * soma;
+
+  int diferenca = quadradoDaSoma - somaDosQuadrados;
+
+  print('Quadrado da soma: $quadradoDaSoma');
+  print('Soma dos quadrados: $somaDosQuadrados');
+  print('Diferença: $diferenca');
+}
