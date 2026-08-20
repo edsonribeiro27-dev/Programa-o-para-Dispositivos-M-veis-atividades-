8abf48d77c1f6281cea8d66f63a4da3cfc68b522
edsonribeiro27-dev <edson.ribeiro62@aluno.ifce.edu.br>
2026-08-20 15:44:15 -0300

    Add functions to calculate sum and average

--- q2.dart
+++ q2.dart
@@ -1,1 +1,17 @@
+int soma(List<int> numeros) {
+  int total = 0;
 
+  for (int numero in numeros) {
+    total += numero;
+  }
+
+  return total;
+}
+
+double media(List<int> numeros) {
+  return soma(numeros) / numeros.length;
+}
+
+void main() {
+  print(media([10, 20, 30]));
+}
