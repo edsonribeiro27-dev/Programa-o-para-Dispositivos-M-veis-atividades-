153052ea0ce35a03688186d83faeacc8126dcd12
edsonribeiro27-dev <edson.ribeiro62@aluno.ifce.edu.br>
2026-08-20 15:45:02 -0300

    Update q4.dart

--- q4.dart
+++ q4.dart
@@ -1,1 +1,20 @@
+List<int> maximo_minimo(List<int> numeros) {
+  int maior = numeros[0];
+  int menor = numeros[0];
 
+  for (int numero in numeros) {
+    if (numero > maior) {
+      maior = numero;
+    }
+
+    if (numero < menor) {
+      menor = numero;
+    }
+  }
+
+  return [maior, menor];
+}
+
+void main() {
+  print(maximo_minimo([4, 7, 2, 8, 10, 3]));
+}
