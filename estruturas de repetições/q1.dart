cd4f717c45c2842bde58493539047f09979918d8
edsonribeiro27-dev <edson.ribeiro62@aluno.ifce.edu.br>
2026-08-20 16:24:24 -0300

    Update q1.dart

--- estruturas de repetições/q1.dart
+++ estruturas de repetições/q1.dart
@@ -1,1 +1,31 @@
+import 'dart:io';
 
+void main() {
+  print('Digite seu nome: ');
+  String nome = stdin.readLineSync()!;
+
+  print('Digite sua idade: ');
+  int idade = int.parse(stdin.readLineSync()!);
+
+  int anoAtual = DateTime.now().year;
+  int anoNascimento = anoAtual - idade;
+
+  print('Olá $nome, você tem $idade anos.');
+  print('Você nasceu em $anoNascimento.');
+
+  for (int ano = anoNascimento; ano <= anoAtual; ano++) {
+    int idadeNoAno = ano - anoNascimento;
+
+    if (idadeNoAno == 0) {
+      print('No ano de $ano você nasceu!');
+    } else if (ano == anoAtual) {
+      print('No ano de $ano você tem $idadeNoAno anos.');
+    } else {
+      print('No ano de $ano você tinha $idadeNoAno anos.');
+    }
+  }
+
+  int anosPara100 = 100 - idade;
+
+  print('Faltam $anosPara100 anos para você completar 100 anos.');
+}
