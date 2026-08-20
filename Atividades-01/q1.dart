6e8c58305d934b634fa1baabb71c900f58983983
edsonribeiro27-dev <edson.ribeiro62@aluno.ifce.edu.br>
2026-08-20 16:13:20 -0300

    Update q1.dart

--- Atividades-01/q1.dart
+++ Atividades-01/q1.dart
@@ -1,1 +1,13 @@
+void main() {
+  int idadeDias = 400;
 
+  int anos = idadeDias ~/ 365;
+  int resto = idadeDias % 365;
+
+  int meses = resto ~/ 30;
+  int dias = resto % 30;
+
+  print('$anos ano(s)');
+  print('$meses mes(es)');
+  print('$dias dia(s)');
+}
