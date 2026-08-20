159eea1432f5c86929603863247ea5265c47bcf8
edsonribeiro27-dev <edson.ribeiro62@aluno.ifce.edu.br>
2026-08-20 16:27:04 -0300

    Add program to find divisors of a number

--- estruturas de repetições/q3.dart
+++ estruturas de repetições/q3.dart
@@ -1,1 +1,14 @@
+import 'dart:io';
 
+void main() {
+  print('Digite um número: ');
+  int numero = int.parse(stdin.readLineSync()!);
+
+  print('Divisores de $numero:');
+
+  for (int i = 1; i <= numero; i++) {
+    if (numero % i == 0) {
+      print(i);
+    }
+  }
+}
