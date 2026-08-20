0d729ed84268300c601b33459811b3a4a7744eac
edsonribeiro27-dev <edson.ribeiro62@aluno.ifce.edu.br>
2026-08-20 16:47:06 -0300

    Update q6.dart

--- Atividades-Funcoes/q6.dart
+++ Atividades-Funcoes/q6.dart
@@ -1,1 +1,22 @@
+String raindrops(int numero) {
+  String resultado = '';
 
+  if (numero % 3 == 0) {
+    resultado += 'Pling';
+  }
+  if (numero % 5 == 0) {
+    resultado += 'Plang';
+  }
+  if (numero % 7 == 0) {
+    resultado += 'Plong';
+  }
+  if (resultado == '') {
+    return numero.toString();
+  }
+  return resultado;
+}
+void main() {
+  print(raindrops(28));
+  print(raindrops(30));
+  print(raindrops(34));
+}
