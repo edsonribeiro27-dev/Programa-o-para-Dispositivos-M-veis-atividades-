51c2f0609efacffc934883ffbb5ae3f91600a4b0
edsonribeiro27-dev <edson.ribeiro62@aluno.ifce.edu.br>
2026-09-02 22:40:52 -0300

    Update q9.dart

--- Atividade de POO/q9.dart
+++ Atividade de POO/q9.dart
@@ -1,1 +1,27 @@
+veterinario.dart
+  import 'animal.dart';
 
+class Veterinario {
+  void examinar(Animal animal) {
+    print('Examinando ${animal.nome}...');
+    animal.emitirSom();
+  }
+}
+
+import 'animal.dart';
+import 'cachorro.dart';
+import 'cavalo.dart';
+import 'preguica.dart';
+import 'veterinario.dart';
+
+void main() {
+  Veterinario veterinario = Veterinario();
+
+  Animal cachorro = Cachorro('Rex', 5);
+  Animal cavalo = Cavalo('Spirit', 7);
+  Animal preguica = Preguica('Lenta', 3);
+
+  veterinario.examinar(cachorro);
+  veterinario.examinar(cavalo);
+  veterinario.examinar(preguica);
+}
