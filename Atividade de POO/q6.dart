177b4906a9976b2aa9e19ec2794b4fc16ec885e8
edsonribeiro27-dev <edson.ribeiro62@aluno.ifce.edu.br>
2026-09-02 22:33:56 -0300

    Update q6.dart

--- Atividade de POO/q6.dart
+++ Atividade de POO/q6.dart
@@ -1,1 +1,48 @@
+import 'empregado.dart';
 
+class Vendedor extends Empregado {
+  double _valorVendas;
+  double _comissao;
+
+  Vendedor()
+      : _valorVendas = 0,
+        _comissao = 0,
+        super();
+
+  Vendedor.completo(
+    String nome,
+    String endereco,
+    String telefone,
+    int codigoSetor,
+    double salarioBase,
+    double imposto,
+    this._valorVendas,
+    this._comissao,
+  ) : super.completo(
+          nome,
+          endereco,
+          telefone,
+          codigoSetor,
+          salarioBase,
+          imposto,
+        );
+
+  double get valorVendas => _valorVendas;
+  double get comissao => _comissao;
+
+  set valorVendas(double valor) {
+    _valorVendas = valor;
+  }
+
+  set comissao(double valor) {
+    _comissao = valor;
+  }
+
+  @override
+  double calcularSalario() {
+    double salario = super.calcularSalario();
+    double valorComissao = _valorVendas * _comissao / 100;
+
+    return salario + valorComissao;
+  }
+}
