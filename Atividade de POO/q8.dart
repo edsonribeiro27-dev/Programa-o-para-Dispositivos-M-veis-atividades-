561009cb918e0c07ac9a8c6c7664c54dfb795303
edsonribeiro27-dev <edson.ribeiro62@aluno.ifce.edu.br>
2026-09-02 22:38:37 -0300

    Update q8.dart

--- Atividade de POO/q8.dart
+++ Atividade de POO/q8.dart
@@ -1,1 +1,14 @@
+import 'animal.dart';
+import 'cachorro.dart';
+import 'cavalo.dart';
+import 'preguica.dart';
 
+void main() {
+  Animal cachorro = Cachorro('Rex', 5);
+  Animal cavalo = Cavalo('Spirit', 7);
+  Animal preguica = Preguica('Lenta', 3);
+
+  cachorro.emitirSom();
+  cavalo.emitirSom();
+  preguica.emitirSom();
+}
