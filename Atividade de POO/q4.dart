aae415db6610fa9dabbcbb2260d728cc51ca7d84
edsonribeiro27-dev <edson.ribeiro62@aluno.ifce.edu.br>
2026-09-02 22:32:52 -0300

    Update q4.dart

--- Atividade de POO/q4.dart
+++ Atividade de POO/q4.dart
@@ -1,1 +1,37 @@
+import 'empregado.dart';
 
+class Administrador extends Empregado {
+  double _ajudaDeCusto;
+
+  Administrador()
+      : _ajudaDeCusto = 0,
+        super();
+
+  Administrador.completo(
+    String nome,
+    String endereco,
+    String telefone,
+    int codigoSetor,
+    double salarioBase,
+    double imposto,
+    this._ajudaDeCusto,
+  ) : super.completo(
+          nome,
+          endereco,
+          telefone,
+          codigoSetor,
+          salarioBase,
+          imposto,
+        );
+
+  double get ajudaDeCusto => _ajudaDeCusto;
+
+  set ajudaDeCusto(double valor) {
+    _ajudaDeCusto = valor;
+  }
+
+  @override
+  double calcularSalario() {
+    return super.calcularSalario() + _ajudaDeCusto;
+  }
+}
