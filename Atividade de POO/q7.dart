565c5c7689b5e13337625b90bd8fecfd0a32bc72
edsonribeiro27-dev <edson.ribeiro62@aluno.ifce.edu.br>
2026-09-02 22:37:45 -0300

    Update q7.dart

--- Atividade de POO/q7.dart
+++ Atividade de POO/q7.dart
@@ -1,3 +1,4 @@
+//animal
 class Animal {
   String nome;
   int idade;
@@ -6,5 +7,53 @@ class Animal {
 
   void emitirSom() {
     print('Som do animal');
+  }
+}
+
+//cachorro
+import 'animal.dart';
+
+class Cachorro extends Animal {
+  Cachorro(String nome, int idade) : super(nome, idade);
+
+  @override
+  void emitirSom() {
+    print('$nome: Au Au!');
+  }
+
+  void correr() {
+    print('$nome está correndo.');
+  }
+}
+
+//cavalo
+import 'animal.dart';
+
+class Cavalo extends Animal {
+  Cavalo(String nome, int idade) : super(nome, idade);
+
+  @override
+  void emitirSom() {
+    print('$nome: Relincho!');
+  }
+
+  void correr() {
+    print('$nome está correndo.');
+  }
+}
+
+//preguica
+import 'animal.dart';
+
+class Preguica extends Animal {
+  Preguica(String nome, int idade) : super(nome, idade);
+
+  @override
+  void emitirSom() {
+    print('$nome: Som da preguiça!');
+  }
+
+  void subirEmArvores() {
+    print('$nome está subindo em árvores.');
   }
 }
