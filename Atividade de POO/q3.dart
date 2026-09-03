a0ccf3e80555bfb0e71f84edafdbe155cbe425d7
edsonribeiro27-dev <edson.ribeiro62@aluno.ifce.edu.br>
2026-09-02 22:31:30 -0300

    Update q3.dart
    Added Empregado class extending Pessoa with salary calculation.

--- Atividade de POO/q3.dart
+++ Atividade de POO/q3.dart
@@ -1,1 +1,42 @@
+import 'pessoa.dart';
 
+class Empregado extends Pessoa {
+  int _codigoSetor;
+  double _salarioBase;
+  double _imposto;
+
+  Empregado()
+      : _codigoSetor = 0,
+        _salarioBase = 0,
+        _imposto = 0,
+        super();
+
+  Empregado.completo(
+    String nome,
+    String endereco,
+    String telefone,
+    this._codigoSetor,
+    this._salarioBase,
+    this._imposto,
+  ) : super.completo(nome, endereco, telefone);
+
+  int get codigoSetor => _codigoSetor;
+  double get salarioBase => _salarioBase;
+  double get imposto => _imposto;
+
+  set codigoSetor(int codigo) {
+    _codigoSetor = codigo;
+  }
+
+  set salarioBase(double salario) {
+    _salarioBase = salario;
+  }
+
+  set imposto(double imposto) {
+    _imposto = imposto;
+  }
+
+  double calcularSalario() {
+    return _salarioBase - (_salarioBase * _imposto / 100);
+  }
+}
