4c07752108a7ca3e76659f68abfadc4dccd22697
edsonribeiro27-dev <edson.ribeiro62@aluno.ifce.edu.br>
2026-09-02 22:30:52 -0300

    Update q2.dart

--- Atividade de POO/q2.dart
+++ Atividade de POO/q2.dart
@@ -1,1 +1,34 @@
+import 'pessoa.dart';
 
+class Fornecedor extends Pessoa {
+  double _valorCredito;
+  double _valorDivida;
+
+  Fornecedor()
+      : _valorCredito = 0,
+        _valorDivida = 0,
+        super();
+
+  Fornecedor.completo(
+    String nome,
+    String endereco,
+    String telefone,
+    this._valorCredito,
+    this._valorDivida,
+  ) : super.completo(nome, endereco, telefone);
+
+  double get valorCredito => _valorCredito;
+  double get valorDivida => _valorDivida;
+
+  set valorCredito(double valor) {
+    _valorCredito = valor;
+  }
+
+  set valorDivida(double valor) {
+    _valorDivida = valor;
+  }
+
+  double obterSaldo() {
+    return _valorCredito - _valorDivida;
+  }
+}
