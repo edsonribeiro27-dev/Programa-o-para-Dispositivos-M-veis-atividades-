ad85d90c9731ea8ebba69c66a397accb61a4feff
edsonribeiro27-dev <edson.ribeiro62@aluno.ifce.edu.br>
2026-09-10 14:07:00 -0300

    Add Cliente class with nome and cpf properties

--- Atividade - Composição/cliente.dart
+++ Atividade - Composição/cliente.dart
@@ -1,1 +1,14 @@
+class Cliente {
+  String nome;
+  int cpf;
 
+  Cliente({
+    required this.nome,
+    required this.cpf,
+  });
+
+  @override
+  String toString() {
+    return 'Nome: $nome | CPF: $cpf';
+  }
+}
